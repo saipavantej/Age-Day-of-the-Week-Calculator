c9cc96c2d05a52f5b3d468a925fe50ae94eafe05
saipavan <saipavantej@gmail.com>
2020-01-01 16:51:03 +0530

    age and day of the week calculator

--- script.js
+++ script.js
@@ -7,27 +7,34 @@ const form = document.getElementById("getAgeForm");
 form.addEventListener("submit", e =>{
     e.preventDefault();
      const inputYear = inputYearElement.value;
-     const inputMonth = inputMonthElement.value;
+     const inputMonth = inputMonthElement.value -1;
      const inputDate =  inputDateElement.value;
-    if(inputYear >=1  && inputMonth >=1  && inputDate >=1 )
+     const date = new Date();
+    if(inputYear <= date.getFullYear() && inputMonth >=0  && inputDate >=1)
     {
      const age = calculateAge(inputYear);
-    //  const day = calculateDay(inputYear,inputMonth,inputDate);
+     const day = calculateDay(inputYear,inputMonth,inputDate);
     
-     displayAgeElement.innerText = age;
-    //  displayDayElement.innerText = day;
+     displayAgeElement.innerText =`${age}`;
+     displayDayElement.innerText = day;
     }
     else
     {
-        alert("Please enter all the input Fields and Try Again");
+        alert("Please valid input Fields and Try Again");
     }
 })
 
 function calculateAge(year){
-    if(year>0)
-    {
+
     const getDate = new Date();
     const age_call = getDate.getFullYear()-year;
     return age_call;
-    }
+    
 }
+ function calculateDay(year,month,day)
+ {
+     const thatDate = new Date(year,month,day);
+     const day_call = thatDate.getDay();
+     const days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"]
+     return days[day_call];
+ }

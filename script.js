15356e363d5a4a16b1d4f7e928f4eaacf45a2e43
saipavan <saipavantej@gmail.com>
2020-01-01 17:45:44 +0530

    added title of the projrct and other some corrections

--- script.js
+++ script.js
@@ -4,37 +4,33 @@ const inputYearElement = document.getElementById("inputYear");
 const inputMonthElement = document.getElementById("inputMonth");
 const inputDateElement = document.getElementById("inputDate");
 const form = document.getElementById("getAgeForm");
-form.addEventListener("submit", e =>{
+form.addEventListener("submit", e => {
     e.preventDefault();
-     const inputYear = inputYearElement.value;
-     const inputMonth = inputMonthElement.value -1;
-     const inputDate =  inputDateElement.value;
-     const date = new Date();
-    if(inputYear <= date.getFullYear() && inputMonth >=0  && inputDate >=1)
-    {
-     const age = calculateAge(inputYear);
-     const day = calculateDay(inputYear,inputMonth,inputDate);
-    
-     displayAgeElement.innerText =`${age}`;
-     displayDayElement.innerText = day;
-    }
-    else
-    {
+    const inputYear = inputYearElement.value;
+    const inputMonth = inputMonthElement.value - 1;
+    const inputDate = inputDateElement.value;
+    const date = new Date();
+    if (inputYear <= date.getFullYear() && inputMonth >= 0 && inputDate >= 1) {
+        const age = calculateAge(inputYear);
+        const day = calculateDay(inputYear, inputMonth, inputDate);
+
+        displayAgeElement.innerText = `${age}`;
+        displayDayElement.innerText = `${day}`;
+    } else {
         alert("Please valid input Fields and Try Again");
     }
 })
 
-function calculateAge(year){
-
+function calculateAge(year) {
     const getDate = new Date();
-    const age_call = getDate.getFullYear()-year;
+    const age_call = getDate.getFullYear() - year;
     return age_call;
-    
+
 }
- function calculateDay(year,month,day)
- {
-     const thatDate = new Date(year,month,day);
-     const day_call = thatDate.getDay();
-     const days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","saturday"]
-     return days[day_call];
- }
+
+function calculateDay(year, month, day) {
+    const thatDate = new Date(year, month, day);
+    const day_call = thatDate.getDay();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "saturday"]
+    return days[day_call];
+}
